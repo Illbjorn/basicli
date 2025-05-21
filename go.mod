6849b2d30c373b2977eac5bfa851479abe42271f
Illbjorn <am@hades.so>
2025-05-21 16:20:44 -0400

    Produce an Error on Undefined Flags

--- go.mod
+++ go.mod
@@ -2,9 +2,6 @@ module github.com/illbjorn/basicli
 
 go 1.24.2
 
-require (
-	github.com/illbjorn/echo v0.7.4
-	gotest.tools/v3 v3.5.2
-)
+require gotest.tools/v3 v3.5.2
 
 require github.com/google/go-cmp v0.5.9 // indirect
--- unmarshal.go
+++ unmarshal.go
@@ -10,83 +10,119 @@ import (
 
 	"github.com/illbjorn/basicli/argv"
 	"github.com/illbjorn/basicli/tag"
-	"github.com/illbjorn/echo"
 )
 
+// Unmarshal `os.Args` input to provided `P` instance `v`.
 func Unmarshal[P *T, T any](v P) error {
+	// Must be a non-nil pointer
 	if v == nil {
-		return fmt.Errorf("received uninitialized ['%T']", v)
+		var v T
+		return fmt.Errorf("received uninitialized [%T]", v)
 	}
+
+	// The pointer must be to a struct
+	rv := Concrete(reflect.ValueOf(v))
+	if rv.Kind() != reflect.Struct {
+		return fmt.Errorf("expected struct, found [%s]", rv.Kind())
+	}
+
 	// Parse args and flags
 	args, flags := argv.Parse(os.Args[1:])
-	return unmarshal(Concrete(reflect.ValueOf(v)), args, flags)
+
+	// Unmarshal and return
+	return unmarshal(rv, args, flags, []string{})
 }
 
-func unmarshal(rv reflect.Value, args []string, flags map[string][]string) error {
-	var arg string
+// unmarshal recursively consumes input `args`, locating a nested struct on `rv`
+// with a case-insensitively-matching name. When found, `unmarshal` recurses to
+// that nested struct until `args` is empty.
+//
+// Once `args` is empty, the leaf struct fields are iterated and any flag values
+// contained in `flags` which match either the struct field name or struct field
+// tag value(s) exactly have the respective provided flag value assigned. This
+// assignment includes conversion of the string input to the data type of the
+// field.
+func unmarshal(rv reflect.Value, args []string, flags map[string][]string, found []string) error {
+	// If we have positional args, locate the nested struct and recurse
 	if len(args) > 0 {
-		arg = args[0]
+		// Slice off the first arg
+		arg := args[0]
 		args = args[1:]
-	}
-
-	for i := range rv.NumField() {
-		field := Concrete(rv.Field(i))
-		fieldType := rv.Type().Field(i)
-
-		if !field.CanAddr() {
-			return fmt.Errorf("found unaddressable field ['%s']", field.Type().Name())
-		}
-
-		// Parse the struct tag (if any)
-		tagStr, tagFound := fieldType.Tag.Lookup(structTag)
-		tag := tag.Parse(tagStr)
-
-		////////////////////////////////////////////////////////////////////////////
-		// Flags
-
-		fieldName := fieldType.Name
-		for k, v := range flags {
-			// First check for a folded match on the map key against the current flag
-			if strings.EqualFold(k, fieldName) {
-				fieldSet(field, v)
+		// Iterate struct fields
+		for i := range rv.NumField() {
+			ft := rv.Type().Field(i)
+			// Look for a struct tag
+			t, ok := ft.Tag.Lookup(structTag)
+			if ok {
+				// Look for a match against the next arg
+				parsed := tag.Parse(t)
+				if slices.Contains(append(parsed.Aliases, parsed.Name), arg) {
+					// Recurse
+					return unmarshal(rv.Field(i), args, flags, found)
+				}
+			}
+			// Look for a case-insensitive match against the field name itself
+			if len(ft.Name) != len(arg) {
 				continue
 			}
-			// If we found a struct tag, check there
-			if tagFound {
-				// Check the primary tag name first
-				if strings.EqualFold(k, tag.Name) {
-					fieldSet(field, v)
-					continue
-				}
-				// Check aliases second
-				if slices.ContainsFunc(tag.Aliases, containsStrFold(k)) {
-					fieldSet(field, v)
-				}
+			if strings.EqualFold(ft.Name, arg) {
+				// Recurse
+				return unmarshal(rv.Field(i), args, flags, found)
 			}
 		}
+		// We failed to locate a nested member for the referenced subcommand
+		return fmt.Errorf("failed to locate subcommand [%s]", arg)
+	}
 
-		////////////////////////////////////////////////////////////////////////////
-		// Subcommands
-
-		// No non-structs beyond this point
-		if len(arg) == 0 || field.Kind() != reflect.Struct {
-			continue
-		}
-
-		// Evaluate a subcommand (arg) match on this field's name or tag values
+	// Otherwise, we're just here to populate field values
+next:
+	for i := range rv.NumField() {
+		ft := rv.Type().Field(i)
+		found = append(found, ft.Name)
+		// Check for a direct match on field name
 		//
-		// If we find one, recurse and populate that member as well
-		if strings.EqualFold(fieldName, arg) {
-			err := unmarshal(field, args, flags)
-			if err != nil {
-				return err
+		// NOTE: Flags are case-sensitive, while subcommands are not
+		flag, ok := flags[ft.Name]
+		if ok {
+			// Set the field value
+			fieldSet(rv.Field(i), flag)
+		}
+		// Also look for a struct tag
+		t, ok := ft.Tag.Lookup(structTag)
+		if ok {
+			tag := tag.Parse(t)
+			// Register "found" flags
+			found = append(found, tag.Name)
+			found = append(found, tag.Aliases...)
+			// Look for a provided flag value which matches
+			for _, name := range append(tag.Aliases, tag.Name) {
+				flag, ok := flags[name]
+				if ok {
+					// Set the field value
+					fieldSet(rv.Field(i), flag)
+					continue next
+				}
 			}
+			// If we made it here and the tag is required, we have a problem
+			if tag.Flags.Required() {
+				fmt.Printf("%#v\n", flags)
+				return fmt.Errorf("flag [%s] is required but was not provided", tag.Name)
+			}
+		}
+	}
+
+	// Confirm we didn't encounter any flags which were not defined on the struct
+	for k := range flags {
+		if !slices.Contains(found, k) {
+			return fmt.Errorf("received unexpected flag [%s]", k)
 		}
 	}
 
 	return nil
 }
 
+// fieldSet evaluates the type of the struct field contained in `rv`, converting
+// `vs` to values of that type then assigning them to the field.
 func fieldSet(rv reflect.Value, vs []string) error {
 	if len(vs) == 0 {
 		return nil
@@ -123,8 +159,7 @@ func fieldSet(rv reflect.Value, vs []string) error {
 		rv.SetUint(ui)
 
 	default:
-		echo.Fatalf("Found unexpected field kind ['%s'].", rv.Kind())
-		panic("")
+		return fmt.Errorf("found unexpected struct field kind [%s]", rv.Kind())
 	}
 
 	return nil
